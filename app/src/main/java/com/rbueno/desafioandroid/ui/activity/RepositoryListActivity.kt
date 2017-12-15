ff527a4e3634708f998885cdf3b7d29e373eb159
Rafael Bueno <rafael.bueno@concrete.com.br>
2017-12-15 10:21:08 -0200

    Atualiza versoes das libs utilizadas

--- app/src/main/java/com/rbueno/desafioandroid/ui/activity/RepositoryListActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.rbueno.desafioandroid.ui.activity
-
-import android.support.v7.app.AppCompatActivity
-
-class RepositoryListActivity : AppCompatActivity() {
-
-}
--- app/src/main/java/com/rbueno/desafioandroid/ui/activity/RepositoryPrListActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.rbueno.desafioandroid.ui.activity
-
-import android.support.v7.app.AppCompatActivity
-
-class RepositoryPrListActivity : AppCompatActivity() {
-
-
-
-}
