eba8e9f1f630791dbe375d7239a1c441777e88ed
Rafael Bueno <rafael.bueno@concrete.com.br>
2017-12-19 09:47:08 -0200

    Adiciona os modelos de resposta da api

---
+++ repository/src/main/kotlin/com/rbueno/desafioandroid/repository/ResponsesModel.kt
@@ -0,0 +1,24 @@
+package com.rbueno.desafioandroid.repository
+
+import com.google.gson.annotations.SerializedName
+
+data class GitRepository(@SerializedName("name") val repositoryName: String,
+                         @SerializedName("description") val repositoryDescription: String,
+                         @SerializedName("forks_count") val repositoryForksCount: Int,
+                         @SerializedName("stargazers_count") val repositoryStarsCount: Int,
+                         @SerializedName("full_name") val repositoryUserName: String,
+                         @SerializedName("owner") val owner: GitRepositoryOwner)
+
+data class GitRepositoryOwner(@SerializedName("login") val ownerLogin: String,
+                              @SerializedName("avatar_url") val avatarUrl: String)
+
+
+data class GitRepositoryPullRequest(@SerializedName("title") val pullRequestTitle: String,
+                                    @SerializedName("body") val pullRequestDescription: String,
+                                    @SerializedName("head") val head: GitRepositoryPullRequestHead)
+
+
+data class GitRepositoryPullRequestHead(@SerializedName("user") val user: GitRepositoryPullRequestUser)
+
+data class GitRepositoryPullRequestUser(@SerializedName("login") val userName: String,
+                                        @SerializedName("avatar_url") val avatarUrl: String)
