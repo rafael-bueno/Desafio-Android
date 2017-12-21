b77c0f082682e8830570a513b80011463a316903
Rafael Bueno <rafael.bueno@concrete.com.br>
2017-12-21 10:44:12 -0200

    Remove call adapter do rx

--- repository/src/main/kotlin/com/rbueno/desafioandroid/repository/ApiService.kt
+++ repository/src/main/kotlin/com/rbueno/desafioandroid/repository/ApiService.kt
@@ -1,13 +1,11 @@
 package com.rbueno.desafioandroid.repository
 
-import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory.createWithScheduler
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ApiService private constructor(){
+class ApiService private constructor() {
 
-    private val repositoryService : RepositoryService
+    private val repositoryService: RepositoryService
 
     companion object {
         val instance = ApiService()
@@ -18,12 +16,11 @@ class ApiService private constructor(){
         val retrofit = Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(createWithScheduler(Schedulers.io()))
                 .build()
 
 
-        repositoryService =  retrofit.create(RepositoryService::class.java)
+        repositoryService = retrofit.create(RepositoryService::class.java)
     }
 
-    fun repositoryService() : RepositoryService = repositoryService
+    fun repositoryService(): RepositoryService = repositoryService
 }
